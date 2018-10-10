14ff6c353cf86ba03643d8c58d7268f9d728257c
Leonardo Parrino <leonardo.parrino@asinfotrack.ch>
2018-10-10 10:08:20 +0200

    bottons arrangement changed

--- views/hyperlink-backend/update.php
+++ views/hyperlink-backend/update.php
@@ -8,23 +8,25 @@ use asinfotrack\yii2\toolbox\widgets\Button;
 $this->title = Yii::t('app', 'Update hyperlink');
 ?>
 
-<?= Button::widget([
-	'tagName'=>'a',
-	'icon'=>'list',
-	'label'=>Yii::t('app', 'All hyperlinks'),
-	'options'=>[
-		'href'=>Url::to(['hyperlink-backend/index']),
-		'class'=>'btn btn-primary',
-	],
-]) ?>
-<?= Button::widget([
-	'tagName'=>'a',
-	'icon'=>'eye',
-	'label'=>Yii::t('app', 'Hyperlink details'),
-	'options'=>[
-		'href'=>Url::to(['hyperlink-backend/view', 'id'=>$model->id]),
-		'class'=>'btn btn-primary',
-	],
-]) ?>
+<div class="buttons">
+	<?= Button::widget([
+		'tagName'=>'a',
+		'icon'=>'list',
+		'label'=>Yii::t('app', 'All hyperlinks'),
+		'options'=>[
+			'href'=>Url::to(['hyperlink-backend/index']),
+			'class'=>'btn btn-primary',
+		],
+	]) ?>
+	<?= Button::widget([
+		'tagName'=>'a',
+		'icon'=>'eye',
+		'label'=>Yii::t('app', 'Hyperlink details'),
+		'options'=>[
+			'href'=>Url::to(['hyperlink-backend/view', 'id'=>$model->id]),
+			'class'=>'btn btn-primary',
+		],
+	]) ?>
+</div>
 
 <?= $this->render('partials/_form', ['model'=>$model]) ?>
